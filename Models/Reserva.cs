825a27b94b24c7cc94f641c4e1bc5a02697cf4bf
fabiodrneles <fabiodrneles@gmail.com>
2023-10-12 13:03:50 -0300

    TODO dos metodos CalcularValorDiaria, ObterQuantidadeHospedes

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -37,8 +37,7 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -46,13 +45,13 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor * 90 / 100;
             }
 
             return valor;
