ae408d1d009fadefed00955432ac038984f9b6e5
fabiodrneles <fabiodrneles@gmail.com>
2023-10-12 13:08:57 -0300

    arrumando calculo de 10% de desconto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -44,11 +44,9 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados >= 10)
             {
                 valor = valor * 90 / 100;
